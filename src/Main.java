b885013577dd15a95601d5bcddafa2e5b4b42cef
Sam Black <sam.black@lubbockisd.org>
2023-09-05 10:02:19 -0500

    notes about data types and identifiers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // variable -- memory location that holds a value
+        // data types -- mission critical
+
+        //identifier rules
+        // -- ALWAYS start with a letter
+        // -- NO spaces
+        // -- CAN use numbers
+        // -- _ is the only symbol allowed
+        // -- CANNOT use Java keywords
+
+        // General conventions
+        // -- classes begin with a Capital
+        // -- camelCase is common for variableNames
+        // -- use identifiers that mean something
+
+        // Data Types
+        // - primatives: store a single, simple value
+        //   8:  4 integers, 2 decimals, 1 boolean (t/f), 1 char
+        //  -- integers: byte, short, int, long
+        //         bits:  8      16    32    64
+        //       byte:  -128 to 127
+        //      short:  -32768 to 32767
+        //        int:  -21474483648 to 2147483647
+        //       long:  -4quintillion to 4quintillion
+
     }
 }
